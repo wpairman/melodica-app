8517152bcee5468139154183e91c8ed658483c7d
wpairman <will7ovo@gmail.com>
2025-12-07 00:48:32 -0500

    Move Quotes link to top of navigation menu for better visibility

--- components/navigation-sidebar.tsx
+++ components/navigation-sidebar.tsx
@@ -62,6 +62,12 @@ export function NavigationSidebar({ isOpen, onOpenChange }: NavigationSidebarPro
                     Home
                   </Button>
                 </Link>
+                <Link href="/dashboard/quotes">
+                  <Button variant="ghost" className="w-full justify-start text-white hover:bg-gray-800">
+                    <Quote className="mr-2 h-4 w-4" />
+                    Quotes
+                  </Button>
+                </Link>
                 <Link href="/calendar">
                   <Button variant="ghost" className="w-full justify-start text-white hover:bg-gray-800">
                     <Calendar className="mr-2 h-4 w-4" />
@@ -116,12 +122,6 @@ export function NavigationSidebar({ isOpen, onOpenChange }: NavigationSidebarPro
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
                     Journaling
-                  </Button>
-                </Link>
-                <Link href="/dashboard/quotes">
-                  <Button variant="ghost" className="w-full justify-start text-white hover:bg-gray-800">
-                    <Quote className="mr-2 h-4 w-4" />
-                    Quotes
                   </Button>
                 </Link>
                 <Link href="/dashboard/jamaica-wellness">
